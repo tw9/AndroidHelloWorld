d9f607186cdf60fd2c022681fae00025656de121
tw9 <taylor@Taylors-MacBook-Pro.local>
2014-08-03 01:42:43 +0800

    button out String to console
    print string to console by click button

--- app/src/main/java/com/example/taylor/helloworld/MyActivity.java
+++ app/src/main/java/com/example/taylor/helloworld/MyActivity.java
@@ -4,14 +4,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MyActivity extends ActionBarActivity {
+
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        button= (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("hello You made it");
+            }
+        });
     }
 
 
